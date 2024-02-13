c2439e462d0b95a76b5ae7b47b5962c06d7e629b
Bruollin <bruollin.s118@gmail.com>
2024-02-13 20:06:57 +0300

    Partie front-end

---
+++ app-angular/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { ClipboardModule } from 'ngx-clipboard';
+
+import { AdminLayoutRoutes } from './admin-layout.routing';
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { IconsComponent } from '../../pages/icons/icons.component';
+import { MapsComponent } from '../../pages/maps/maps.component';
+import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
+import { AccueilComponent } from 'src/app/pages/accueil/accueil.component';
+import { TablesComponent } from '../../pages/tables/tables.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ListeServicesComponent } from 'src/app/pages/liste-services/liste-services.component';
+import { CalendrierComponent } from 'src/app/pages/accueil/calendrier/calendrier.component';
+import { HistoriqueComponent } from 'src/app/pages/accueil/historique/historique.component';
+// import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(AdminLayoutRoutes),
+    FormsModule,
+    HttpClientModule,
+    NgbModule,
+    ClipboardModule
+  ],
+  declarations: [
+    DashboardComponent,
+    UserProfileComponent,
+    TablesComponent,
+    IconsComponent,
+    MapsComponent,
+    AccueilComponent,
+    ListeServicesComponent,
+    CalendrierComponent,
+    HistoriqueComponent
+  ]
+})
+
+export class AdminLayoutModule {}
