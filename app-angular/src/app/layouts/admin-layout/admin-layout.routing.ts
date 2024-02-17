77cb8dd61d66e05750b009ebd8686305102851fa
Bruollin <bruollin.s118@gmail.com>
2024-02-17 16:14:38 +0300

    Détail service

--- app-angular/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ app-angular/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -6,6 +6,7 @@ import { MapsComponent } from '../../pages/maps/maps.component';
 import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
 import { TablesComponent } from '../../pages/tables/tables.component';
 import { AccueilComponent } from 'src/app/pages/accueil/accueil.component';
+import { DetailServiceComponent } from 'src/app/pages/liste-services/detail-service/detail-service.component';
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'dashboard',      component: DashboardComponent },
@@ -13,5 +14,6 @@ export const AdminLayoutRoutes: Routes = [
     { path: 'user-profile',   component: UserProfileComponent },
     { path: 'tables',         component: TablesComponent },
     { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent }
+    { path: 'maps',           component: MapsComponent },
+    { path: 'detail-service',           component: DetailServiceComponent }
 ];
