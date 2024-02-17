2be291ab230d775ae5e7093bbb97fca965859134
Bruollin <bruollin.s118@gmail.com>
2024-02-17 11:11:41 +0300

    Modification service

--- back-end/src/models/service.model.js
+++ back-end/src/models/service.model.js
@@ -1,14 +1,21 @@
 const mongoose = require('mongoose');
 
 const serviceSchema = new mongoose.Schema({
+    id: { type: Number, required: true, unique: true },
     name: String,
     duration: Number,
     price: Number,
     commission_pourcentage: Number,
     date_insertion: { type: Date, default: Date.now },
     etat: { type: Boolean, default: true },
-    // autres champs
 });
 
+serviceSchema.pre('save', async function (next) {
+    if (!this.id) {
+        const lastService = await Service.findOne({}, {}, { sort: { 'id': -1 } });
+        this.id = (lastService && lastService.id + 1) || 1;
+    }
+    next();
+});
 
 module.exports = mongoose.model('Service', serviceSchema);
