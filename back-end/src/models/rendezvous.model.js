beb9b92530fdaa974a920b4c5bd0c8485ff3632e
noutahiana <noutahiana01@gmail.com>
2024-02-17 11:58:39 +0100

    modification service

--- back-end/src/models/rendezvous.model.js
+++ back-end/src/models/rendezvous.model.js
@@ -4,12 +4,11 @@ const rendezSchema = new mongoose.Schema({
     client_id : { type: Schema.Types.ObjectId, ref: 'User' },
     employee_id:{ type: Schema.Types.ObjectId, ref: 'User' },
     service_id: { type: Schema.Types.ObjectId, ref: 'Service' },
+    date_rendez_vous:{ type: Date},
+    heure_rendez_vous:{ type: String, default: '00:00' },
     date_insertion: { type: Date, default: Date.now },
-    status: String,
-    price: Number,
-    etat: { type: Boolean, default: true },
+    etat: { type: Number}, //1-En attente 2-En cours 3-terminer 4-Annuler
     
 });
-
 
 module.exports = mongoose.model('rendezvous', rendezSchema);
--- back-end/src/models/service.model.js
+++ back-end/src/models/service.model.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const serviceSchema = new mongoose.Schema({
-    id: { type: Number, required: true, unique: true },
     name: String,
     duration: Number,
     price: Number,
@@ -9,13 +8,4 @@ const serviceSchema = new mongoose.Schema({
     date_insertion: { type: Date, default: Date.now },
     etat: { type: Boolean, default: true },
 });
-
-serviceSchema.pre('save', async function (next) {
-    if (!this.id) {
-        const lastService = await Service.findOne({}, {}, { sort: { 'id': -1 } });
-        this.id = (lastService && lastService.id + 1) || 1;
-    }
-    next();
-});
-
 module.exports = mongoose.model('Service', serviceSchema);
