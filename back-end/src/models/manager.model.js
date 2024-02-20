131d04231bee98c8d3bb2576c97d532c29316241
noutahiana <noutahiana01@gmail.com>
2024-02-20 10:03:32 +0100

    Modif manager

--- back-end/src/models/manager.model.js
+++ back-end/src/models/manager.model.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const managerSchema = new mongoose.Schema({
-    manager_id: { type: Schema.Types.ObjectId, ref: 'User' },
-    average_work_time: Number,//temps de travail moyen
-    reservations_per_day: Number,//reservation par jour
-    reservations_per_month: Number,//reservation par moi
-    revenue_per_day: Number,
-    revenue_per_month: Number,
-    profit_per_month: Number,
+    firstname: String,
+    lastname: String,
+    email: String,
+    password: String,
     date_insertion: { type: Date, default: Date.now },
-    etat: { type: Boolean, default: true },
+    etat: { type: Number}
 });
-
 
 module.exports = mongoose.model('Manager', managerSchema);
