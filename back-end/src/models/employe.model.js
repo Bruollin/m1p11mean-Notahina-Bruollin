43eeb1f5c11670ec0b5aa6a911f94fd6300970f1
Bruollin <bruollin.s118@gmail.com>
2024-02-22 18:44:01 +0300

    Modif collection Employe

--- back-end/src/models/employe.model.js
+++ back-end/src/models/employe.model.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const employeSchema = new mongoose.Schema({
     service_id: { type: Schema.Types.ObjectId, ref: 'Service' },
-    Name : String,
+    firstname: String,
+    lastname: String,
     email: String,
     password: String,
     preferences : Number,
