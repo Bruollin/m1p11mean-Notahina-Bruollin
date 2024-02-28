311f6827e234deca1beb9563af52f25c29f7aa23
Bruollin <bruollin.s118@gmail.com>
2024-02-28 23:11:38 +0300

    Mise à jour de l'utilisateur

--- app-angular/src/app/pages/user-profile/user-profile.component.ts
+++ app-angular/src/app/pages/user-profile/user-profile.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { API_BASE_URL } from 'src/app/api-config';
+import { AuthService } from 'src/app/components/navbar/auth.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  loggedInUser : any;
+  constructor(private http: HttpClient, private auth: AuthService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.loggedInUser = this.auth.getLoggedInUser();
+  }
+  updateUserData(): void {
+    const userId = this.loggedInUser._id;
+    const updatedUserData = {
+      firstname: this.loggedInUser.firstname,
+      lastname: this.loggedInUser.lastname,
+      email: this.loggedInUser.email,
+      num_phone: this.loggedInUser.num_phone,
+      address: this.loggedInUser.address,
+      gender: this.loggedInUser.gender
+    };
+
+    this.http.put<any>(API_BASE_URL+'utilisateurs/'+userId, updatedUserData)
+      .subscribe(
+        response => {
+          console.log('User data updated successfully:', response);
+        },
+        error => {
+          console.error('Error updating user data:', error);
+        }
+      );
   }
 
 }
